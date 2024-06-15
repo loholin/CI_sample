2264fcc8612a7439144a8c638294e7270c86ed80
Hj11 <hojin2661@naver.com>
2024-06-15 13:33:33 +0900

    first step

--- build.gradle.kts
+++ build.gradle.kts
@@ -19,6 +19,7 @@ dependencies {
 tasks.test {
     useJUnitPlatform()
     finalizedBy("jacocoTestReport") // test 작업 후에 jacocoTestReport 작업 실행
+    ignoreFailures = true // 테스트 실패를 무시하고 계속 진행
     reports {
         junitXml.required.set(true) // JUnit XML 보고서 생성
         html.required.set(true) // HTML 보고서 생성
