e5dcdf8a82fcca78b13c50b57bb2fdd1c22486f0
Hj11 <hojin2661@naver.com>
2024-06-15 14:23:28 +0900

    first step

--- build.gradle.kts
+++ build.gradle.kts
@@ -13,26 +13,41 @@ repositories {
 dependencies {
     testImplementation(platform("org.junit:junit-bom:5.9.1"))
     testImplementation("org.junit.jupiter:junit-jupiter")
-    testImplementation ("org.mockito:mockito-core:3.+")
+    testImplementation("org.mockito:mockito-core:3.+")
 }
 
 tasks.test {
     useJUnitPlatform()
-    finalizedBy("jacocoTestReport") // test 작업 후에 jacocoTestReport 작업 실행
-   ignoreFailures = true // 테스트 실패를 무시하고 계속 진행
+    finalizedBy(tasks.jacocoTestReport) // test 작업 후에 jacocoTestReport 작업 실행
+    ignoreFailures = true // 테스트 실패를 무시하고 계속 진행
     reports {
         junitXml.required.set(true) // JUnit XML 보고서 생성
         html.required.set(true) // HTML 보고서 생성
     }
 }
 
-tasks.named("jacocoTestReport", JacocoReport::class.java) {
+tasks.jacocoTestReport {
     dependsOn(tasks.test)
 
     reports {
         xml.required.set(true)
         html.required.set(true)
         html.outputLocation.set(file("${buildDir}/reports/jacoco/test/html"))
+    }
+}
+
+tasks.register<JacocoCoverageVerification>("jacocoTestCoverageVerification") {
+    dependsOn(tasks.jacocoTestReport)
+
+    violationRules {
+        rule {
+            element = "CLASS"
+            limit {
+                counter = "LINE"
+                value = "COVEREDRATIO"
+                minimum = "0.80".toBigDecimal() // 최소 커버리지 기준 설정
+            }
+        }
     }
 }
 
